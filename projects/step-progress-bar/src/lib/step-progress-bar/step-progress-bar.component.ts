095d06fd094a806665f0dc4b6b35f1f3bf611e7a
Erivan <erivan.nas@gmail.com>
2020-03-10 10:29:29 -0300

    Adiciona métodos para avançar e resetar

--- projects/step-progress-bar/src/lib/step-progress-bar/step-progress-bar.component.ts
+++ projects/step-progress-bar/src/lib/step-progress-bar/step-progress-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { StepItem } from '../model/StepItem';
 
 @Component({
@@ -6,14 +6,51 @@ import { StepItem } from '../model/StepItem';
   templateUrl: './step-progress-bar.component.html',
   styleUrls: ['./step-progress-bar.component.scss']
 })
-export class StepProgressBarComponent implements OnInit {
+export class StepProgressBarComponent implements OnInit, OnChanges {
+
 
   @Input("items") items: Array<StepItem>;
+  @Input('steps') steps: number;
+  private currentStep= 0;
 
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes.steps)
+      this.resolveItems(Number(changes.steps.currentValue));
+  }
+
+  resolveItems(steps: number) {
+    this.items = [];
+    for(let i = 0; i < steps; i++) {
+      this.items.push({value: i+ 1 +'', status:"pendent", isFirst: false, isLast: false})
+    }
+  }
+
+  reset() {
+    this.items.forEach(item => {
+        item.status = "pendent"
+    });
+    this.currentStep = 0;
+  }
+
+  setSuccess(): void {
+    this.advance("success");
+  }
+
+  setFail(): void {
+    this.advance("fail");
+  }
+
+  private advance(status: string) {
+    if(this.currentStep < this.items.length) {
+      this.items[this.currentStep].status = status;
+      this.currentStep++;
+    }
   }
 
   getWidth() {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { StepItem } from 'projects/step-progress-bar/src/lib/model/StepItem';
+import { StepProgressBarComponent } from 'projects/step-progress-bar/src/lib/step-progress-bar/step-progress-bar.component';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +10,24 @@ import { StepItem } from 'projects/step-progress-bar/src/lib/model/StepItem';
 export class AppComponent {
 
   items: Array<StepItem> = [];
+  @ViewChild(StepProgressBarComponent, {static: false}) stepProgressBarComponent: StepProgressBarComponent;
+
   
   constructor() {
     this.items.push({value: "1", status:"success", isFirst: true, isLast: false});
     this.items.push({value: "2", status:"success", isFirst: false, isLast: false});
     this.items.push({value: "3", status:"pendent", isFirst: false, isLast: true});
+  }
+
+  setSuccess() {
+    this.stepProgressBarComponent.setSuccess();
+  }
+
+  setFail() {
+    this.stepProgressBarComponent.setFail();
+  }
+
+  reset() {
+    this.stepProgressBarComponent.reset();
   }
 }
